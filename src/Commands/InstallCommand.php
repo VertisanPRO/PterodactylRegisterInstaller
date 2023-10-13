a561f4395e9b72627458835fcd8603612ebb97bb
PadowYT2 <me@padow.ru>
2023-10-13 23:47:51 +0300

    Test the prompts class

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -4,6 +4,7 @@ namespace Wemx\PterodactylRegister\Commands;
 
 use Illuminate\Console\Command;
 use GuzzleHttp\Client;
+use Laravel\Prompts;
 
 class InstallCommand extends Command
 {
@@ -14,39 +15,52 @@ class InstallCommand extends Command
 
     public function handle()
     {
+        $progress = Prompts::progress(label: 'Updating users', steps: 5, hint: 'This may take a while');
+        $progress->start();
+
         $client = new Client();
 
         $res = $client->get('https://raw.githubusercontent.com/VertisanPRO/PterodactylRegister/LoginContainer/resources/scripts/components/auth/LoginContainer.tsx');
         if (sha1($res->getBody()) == sha1(file_get_contents('resources/scripts/components/auth/LoginContainer.tsx'))) {
+            $progress->finish();
             return $this->fail('You already have the Register Module installed');
         }
 
         $this->info('Starting the installation of the Register Module');
+        $progress->advance();
 
         $res = $client->get('https://raw.githubusercontent.com/pterodactyl/panel/v1.11.3/resources/scripts/components/auth/LoginContainer.tsx');
         if (sha1($res->getBody()) !== sha1(file_get_contents('resources/scripts/components/auth/LoginContainer.tsx'))) {
+            $progress->finish();
             return $this->fail('Detected changed file');
         }
 
         exec('curl --silent -L https://github.com/VertisanPRO/PterodactylRegister/releases/latest/download/RegisterModule.tar.gz | tar -xzv');
         exec('curl --silent -L https://github.com/VertisanPRO/PterodactylRegister/releases/latest/download/LoginContainer.tar.gz | tar -xzv');
+        $progress->advance();
 
         $this->info('Clearing cache');
         exec('php artisan optimize');
+        $progress->advance();
 
         $output = null;
         exec("yarn", $output, $return_var);
+        $progress->advance();
 
         if ($return_var === 1) {
+            $progress->finish();
             return $this->fail('Yarn is not installed');
         }
 
         if (!strpos($output[1], 'Validating package.json')) {
+            $progress->finish();
             return $this->fail('Detected cmdtest package installed');
         }
 
         $this->info('Building assets (this may take a while)');
+        $progress->advance();
         exec('yarn build:production');
+        $progress->finish();
         return $this->info('Successfully installed the Register Module for Pterodactyl. If you have any questions or issues, please reach out to our Discord Server - https://discord.gg/RJfCxC2W3e');
     }
 
